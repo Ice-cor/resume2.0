9c47d3e26b3e5fad0616fc7522c5e5bfb52e9b64
ZWQ <zhong0102@qq.com>
2018-05-30 00:44:19 +0800

    loading改进、logo

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+# resume 0.1
+静态页面搭建
+
+# resume1.0
+
+功能描述
+1.原生JS实现loading、缓动动画、无缝轮播、留言板
+2.引入了tween.js实现动画曲线，leanCloud后端数据库
+3.MVC思想重构代码
+
+遗留问题
+1.鼠标滚轮持续滑动页面会滑动失败
+2.整体UI样式需改版
+3.缺失功能补全（PDF下载、留言数限制）
+
 # resume2.0
+
+优化
+1.引入swiper.js实现轮播切换
+2.引入particles.js改进loading效果
+3.改进logo
--- js/loading.js
+++ js/loading.js
@@ -7,21 +7,29 @@
         init: function (view) {
             this.view = view
             // this.addClass()
+            this.particlesInit()
             this.bindEvents()
             
         },
         bindEvents: function () {
-            this.timeId = setTimeout(this.addClass.bind(this),1000)
+            // this.timeId = setTimeout(this.addClass.bind(this),1000)
+            document.querySelector('.enter').onclick = function(){
+                controller.addClass()
+            }
         },
         addClass: function () {
             view.classList.add('active')
-            this.timeId = null
+            // this.timeId = null
+        },
+        particlesInit: function(){
+            particlesJS.load('loadingWrap', './particlesjs-config.json', function () {
+                console.log('callback - particles.js config loaded');
+            });
         }
 
     }
     controller.init(view)
 
     // console.log('start')
-
 
 }).call() //页面loading效果
